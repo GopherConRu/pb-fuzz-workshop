f83150f854804b65b5aa129ae4abf7a3e27bcf80
Valery Meleshkin <valery@meleshk.in>
2021-04-25 16:14:57 +0300

    reverse 2 props

--- reverse_rapid_test.go
+++ reverse_rapid_test.go
@@ -1,6 +1,33 @@
 package pb_fuzz_workshop
 
 import (
-	_ "github.com/stretchr/testify/require"
-	_ "pgregory.net/rapid"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"pgregory.net/rapid"
 )
+
+func TestReverseNaive(t *testing.T) {
+	rapid.Check(t, func(t *rapid.T) {
+		// generate data
+		s := rapid.SliceOf(rapid.Int()).Draw(t, "slice").([]int)
+
+		// run prop.
+		require.Equal(t, s, Reverse(Reverse(s)))
+	})
+}
+
+func TestReverse(t *testing.T) {
+	rapid.Check(t, func(t *rapid.T) {
+		s := rapid.SliceOf(rapid.Int()).Draw(t, "slice").([]int)
+
+		orig := make([]int, len(s))
+		require.Equal(t, len(s), copy(orig, s))
+
+		reversed := Reverse(s)
+
+		for i := range orig {
+			require.Equal(t, orig[i], reversed[len(orig)-1-i])
+		}
+	})
+}
